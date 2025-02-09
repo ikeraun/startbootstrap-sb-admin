92f76d85002154f061edc2a8386d2b49a037ad32
virginia-zuror <virginiazuor@gmail.com>
2025-02-09 21:35:14 +0100

    basic forms

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -53,4 +53,31 @@
     e.preventDefault();
   });
 
+  $(document).ready(function () {
+    $(".js-user-row").click(function (ev) {
+     $('#user-modal').modal('show')
+    });
+  });
+
+  $('#user-modal').on('hide', function () {
+    $('#user-modal').modal('refresh')
+  })
+
+  (function() {
+    window.addEventListener('load', function() {
+  
+      var forms = document.getElementsByClassName('needs-validation');
+      
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
 })(jQuery); // End of use strict
